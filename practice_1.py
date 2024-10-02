1be51604cf2d7b38f4c37afa98d61f988e4cc943
Hitomi Uchihi <pecobitaro@gmail.com>
2024-10-03 07:51:58 +0900

    fibonacci

--- practice_1.py
+++ practice_1.py
@@ -1,17 +1,23 @@
 # ここに回答を作成
-# 問題1
+# 問題1:整数が与えられたとき、その数が素数かどうかを判定する関数 is_prime(n)
 def is_prime(n):
-    array = []
+    print(f"n:{n}")
     if n < 2:
         return False
     elif n >= 2:
-        for i in range(2, n + 1):
+        for i in range(2, int(n/2) +1):
+            print(f"i:{i}")
+            print(f"n%i:{n%i}")
             if n % i == 0:
-                array.append(i)
-        if len(array) == 1:
-            return True
-        else:
-            return False
+                return False
+    return True
     
-print(is_prime(11))  # True
-print(is_prime(4))   # False
+#print(is_prime(11))  # True
+#print(is_prime(4))   # False
+
+# 問題2:N 番目のフィボナッチ数を返す関数 fibonacci(n)
+def fibonacci(n):
+    return "fib"
+
+
+print(fibonacci(10))  # 55
