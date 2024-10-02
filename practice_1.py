0aec67ca18288e53d5a60057da853d78f00e3279
Hitomi Uchihi <pecobitaro@gmail.com>
2024-10-03 07:23:38 +0900

    1万

--- practice_1.py
+++ practice_1.py
@@ -1,1 +1,17 @@
 # ここに回答を作成
+# 問題1
+def is_prime(n):
+    array = []
+    if n < 2:
+        return False
+    elif n >= 2:
+        for i in range(2, n + 1):
+            if n % i == 0:
+                array.append(i)
+        if len(array) == 1:
+            return True
+        else:
+            return False
+    
+print(is_prime(11))  # True
+print(is_prime(4))   # False
